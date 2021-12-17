eef8db861b4d302f0c1dda83c86107802c3020a0
EddyBautista <bautista.j.eddy@gmail.com>
2021-12-16 20:17:07 -0600

    start work on ResercationController

---
+++ APIControllers/Controllers/ReservationController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using APIControllers.Models;
+using Microsoft.Extensions.Primitives;
+
+namespace APIControllers.Controllers
+{
+    // used to serve http API responses 
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ReservationController : ControllerBase
+    {
+        private IRepository respository;
+        public ReservationController(IRepository repo) => respository = repo;
+        
+    }
+}
