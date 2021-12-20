69e03ed2e8507883b95f7d5baed50757fccf5e11
EddyBautista <bautista.j.eddy@gmail.com>
2021-12-20 07:25:36 -0600

    add remaining controller actions

--- APIControllers/Controllers/ReservationController.cs
+++ APIControllers/Controllers/ReservationController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using APIControllers.Models;
 using Microsoft.Extensions.Primitives;
+using Microsoft.AspNetCore.JsonPatch;
 
 namespace APIControllers.Controllers
 {
@@ -13,8 +14,42 @@ namespace APIControllers.Controllers
     [Route("api/[controller]")]
     public class ReservationController : ControllerBase
     {
-        private IRepository respository;
-        public ReservationController(IRepository repo) => respository = repo;
+        private IRepository repository;
+        public ReservationController(IRepository repo) => repository = repo;
+        [HttpGet]
+        public IEnumerable<Reservation> Get() => repository.Reservations;
+
+        [HttpGet("{id}")]
+        public ActionResult<Reservation> Get(int id)
+        {
+            if (id == 0)
+                return BadRequest("Value must be passed in the request body");
+            return Ok(repository[id]);
+        }
+        [HttpPost]
+        public Reservation Post([FromBody] Reservation res) => repository.AddReservation(new Reservation
+        {
+            Name = res.Name,
+            StartLocation = res.StartLocation,
+            EndLocation = res.EndLocation
+        });
         
+        [HttpPut]
+        public Reservation Put([FromBody] Reservation res) => repository.UpdateReservation(res);
+
+        [HttpPatch("{id}")]
+        public StatusCodeResult Patch(int id, [FromBody] JsonPatchDocument<Reservation> patch)
+        {
+            var res = (Reservation)((OkObjectResult)Get(id).Result).Value;
+            if (res != null)
+            {
+                patch.ApplyTo(res);
+                return Ok();
+            }
+            return NotFound();
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id) => repository.DeleteReservation(id);
     }
 }
